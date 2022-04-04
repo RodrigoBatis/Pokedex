e1fc9383e3b7e077c256970dcdc33096be46ed3a
RodrigoBatis <rodrigofreirebatista@gmail.com>
2022-04-04 13:45:22 -0300

    <Finalizado>

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const pesquisarPoke = () =>{
+
+    const getPokemonurl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    for (let i =1; i<=151; i++){
+        pokemonPromises.push(fetch(getPokemonurl(i)).then(response => response.json()))
+    }
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+        const lisPokemons = pokemons.reduce((accumulator ,pokemon) => {
+
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            accumulator += 
+            `<div class="card">
+                <div class ="img">
+                <img class ="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+                </div>
+                <div class ="atributos">
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card=subtitle">${types.join(' | ')}</p>
+                </div>
+                
+            </div>`
+            return accumulator
+        }, '')
+        const ul = document.querySelector('[data-js="pokedex"]')
+       
+        ul.innerHTML = lisPokemons
+    })
+}
+
+pesquisarPoke()
+
